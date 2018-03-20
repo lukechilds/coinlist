66e753923faf4ca0a9774e69ec5d7e8c75c8489b
Luke Childs <lukechilds123@gmail.com>
2018-03-20 14:05:47 +0700

    Test coin.get helper method seaarches symbols

--- test/index.js
+++ test/index.js
@@ -14,3 +14,14 @@ test('coin object has expected properties', t => {
 	const actualKeys = Object.keys(coins[0]);
 	t.deepEqual(expectedKeys, actualKeys);
 });
+
+test('coin.get helper method seaarches symbols', t => {
+	const expectedBtc = {
+		id: 'bitcoin',
+		symbol: 'BTC',
+		name: 'Bitcoin',
+		maxSupply: 21000000
+	};
+	const actualBtc = coins.get('BTC');
+	t.deepEqual(expectedBtc, actualBtc);
+});
