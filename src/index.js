199af4b8c6e0134b9c6bb269420268163f14747b
Luke Childs <lukechilds123@gmail.com>
2018-03-20 14:53:06 +0700

    Allow getting a property

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@
 
 const coins = require('./coins.json');
 
-coins.get = symbol => coins.find(coin => coin.symbol.toUpperCase() === symbol.toUpperCase());
+coins.get = (symbol, prop) => {
+	const coin = coins.find(coin => coin.symbol.toUpperCase() === symbol.toUpperCase());
+
+	if (typeof prop === 'undefined' || typeof coin === 'undefined') {
+		return coin;
+	}
+
+	const value = coin[prop];
+
+	if (typeof value === 'undefined') {
+		throw new TypeError(`Invalid property: ${prop}`);
+	}
+
+	return value;
+};
 
 module.exports = coins;
