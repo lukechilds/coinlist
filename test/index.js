cdc932f9ade512a10bb0721d3a0a0132c9d5c454
Luke Childs <lukechilds123@gmail.com>
2018-03-20 13:57:15 +0700

    Test coin object has expected properties

--- test/index.js
+++ test/index.js
@@ -8,3 +8,9 @@ test('coins is an array', t => {
 test('coins contains more than 0 coins', t => {
 	t.true(coins.length > 0);
 });
+
+test('coin object has expected properties', t => {
+	const expectedKeys = ['id', 'symbol', 'name', 'maxSupply'];
+	const actualKeys = Object.keys(coins[0]);
+	t.deepEqual(expectedKeys, actualKeys);
+});
