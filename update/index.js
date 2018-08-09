cfeb655eff0a46eec1973a0bd0a50e62ba5b9be9
Sindre Sorhus <sindresorhus@gmail.com>
2018-08-09 15:33:06 +0700

    Switch to coingecko.com
    Context: https://github.com/atomiclabs/hyperdex/issues/464

--- update/index.js
+++ update/index.js
@@ -6,13 +6,15 @@ const writeJsonFile = require('write-json-file');
 const jsonFile = 'src/coins.json';
 
 (async () => {
-	console.log(`Fetching latest currencies from the coinmarketcap.com API...`);
-	const response = await got('https://api.coinmarketcap.com/v2/listings/', { json: true });
-	const coins = response.body.data.map(coin => {
-		const { id, symbol, name } = coin;
-
-		return { id, symbol, name };
-	});
+	console.log(`Fetching latest currencies from the coingecko.com API...`);
+	const response = await got('https://api.coingecko.com/api/v3/coins/list', { json: true });
+	const coins = response.body
+		.map(coin => ({
+			id: coin.id,
+			symbol: coin.symbol.toUpperCase(),
+			name: coin.name
+		}))
+		.sort((a, b) => a.id.localeCompare(b.id));
 	await writeJsonFile(jsonFile, coins);
 	console.log(`Written ${coins.length} coins to ${jsonFile}`);
 })();
